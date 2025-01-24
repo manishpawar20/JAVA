f1b76e2a3ed770198c5c102f2d460da776a8ab52
Manish Pawar <mspawar4620@gmail.com>
2025-01-24 09:48:33 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        //Taking name marks from user and printing the totalMarks and percentage of that student
+//        System.out.println("Enter Your name: ");
+//        String name = sc.next();
+//        System.out.println("Enter Your Marks of 5 subjects: ");
+//        float a = sc.nextFloat();
+//        float b = sc.nextFloat();
+//        float c = sc.nextFloat();
+//        float d = sc.nextFloat();
+//        float e = sc.nextFloat();
+//        double totalMarks = a+b+c+d+e;
+//        double percentage = (totalMarks/5.0);
+//        //----------------OUTPUT--------------
+//        System.out.println("---------Student Details----------");
+//        System.out.println("Name: "+name+"\nTotal Marks: "+totalMarks+"\nPercentage: "+percentage);
+
+        //Write a program that converts temperatures between Celsius and Fahrenheit.
+//        System.out.print("Enter value in Celsius to convert into Fahrenheit: ");
+//        double D = sc.nextDouble();
+//        double F = ((D*(9/5.0))+32);
+//        System.out.println("Celsius = "+D+"C --> Fahrenheit = "+F);
+
+//      Create a program that reads a student's score and determines their letter grade (A, B, C, D, or F) based on a grading scale.
+//        System.out.println("Enter your name: ");
+//        String name = sc.next();
+//        System.out.println("Enter your Percentage (Out of 100): ");
+//        float grade = sc.nextFloat();
+//        if(grade<0 && grade>100)
+//        {
+//           System.out.println("Invalid Grades!!!!");
+//        } else if (grade > 80 && grade <= 100) {
+//            System.out.println("Grade : A");
+//        } else if(grade > 60 && grade <= 80) {
+//            System.out.println("Grade : B");
+//        } else if (grade > 50 && grade <= 60) {
+//            System.out.println("Grade : C");
+//        } else if (grade >= 40 && grade <= 50) {
+//            System.out.println("Grade : D");
+//        } else {
+//            System.out.println("Grade : F");
+//        }
+    }
+}
+
+// Scanner is a class and we create an instance of a class.

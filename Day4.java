7b607ed550eb847c722deebf42e6f7e568f24e7c
Manish Pawar <mspawar4620@gmail.com>
2025-01-26 18:55:23 +0530

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Day4 {
+    public static void main(String[] args){
+//        Today solving some question on string .
+//        Before that remember, String is a class in JAVA, but we can use string as a primitive datatype.
+//        Note That !!
+//        A String is a sequence of characters and string is immutable that means it is unchangeable.
+//        Lets start some Question!
+//
+//        This is the continuation of Day3 and
+//        I am going to solve some question from chatgpt and some from CWH's Practice set.
+        Scanner sc = new Scanner(System.in);
+
+//        Q 1 : Write a program to check if the following string starts with "Java" using .startsWith():
+//        Given string :String str = "Java Programming is powerful";
+//        String str = "Java Programming is powerful";
+//        if(str.startsWith("Java"))
+//        {
+//            System.out.println("Yes,It starts .");
+//        } else {
+//            System.out.println("No,It didn't .");
+//        }
+
+
+//        Q 2 : Write a program to check if the following string ends with "ful" using .endsWith():
+//        String str = "Learning is meaningful";
+//        if(str.endsWith("ful"))
+//        {
+//            System.out.println("Yes,It ends .");
+//        } else {
+//            System.out.println("No,It didn't .");
+//        }
+
+
+//        Q 3 : Write a program to find the character at the 5th position (index 4) in the following string using .charAt():
+//        Given String : String str = "Hello World";
+//        String str = "Hello World";
+//        System.out.println(str.charAt(4));
+//      !! String Starts from 0 index !!
+
+
+//         Q 4 : Write a program to find the index of the first occurrence of the letter 'a' in the following string using .indexOf():
+//         Given String : String str = "Java Programming Language";
+//         String str = "Java Programming Language";
+//         System.out.println(str.indexOf("a"));
+//         System.out.println(str.indexOf("s"));  // It will return -1 if the ch is note in the string or str.
+
+
+//         Q 5 : Write a program to find the index of the last occurrence of the letter 'a' in the following string using .lastIndexOf():
+//         String str = "Java Programming Language";
+//         System.out.println(str.lastIndexOf('a'));
+
+//         Q 6 : Write a program to check if the two strings are equal using .equals():
+//        String str1 = "Hello";
+//        String str2 = "hello";
+//        if(str1.equals(str2)) // It did not ignore the case sensitivity
+//        {
+//            System.out.println("They are equal.");
+//        } else {
+//            System.out.println("They are not equal.");
+//        }
+
+
+//        Q 7 : Write a program to check if the two strings are equal (case-insensitive) using .equalsIgnoreCase():
+//        String str1 = "Hello";
+//        String str2 = "HELlO";
+//        if(str1.equalsIgnoreCase(str2)) // It ignore the case sensitivity
+//        {
+//            System.out.println("They are equal.");
+//        } else {
+//            System.out.println("They are not equal.");
+//        }
+
+//        Problems on Escape Sequence Characters
+
+//        Using some Escape Sequence Characters
+        System.out.println("Hello\nI am Manish\nI love to paly \"Video Games\" and \t love to code");
+/*
+* \n -> new line
+* \t -> to give space like tab
+* and we can use \ to use single and double quotes in our String statement.
+*/
+
+
+
+
+
+    }
+}
---
+++ Day4CWHpracticeSet.java
@@ -0,0 +1,58 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Day4CWHpracticeSet {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        Q 1 : Write a Java program to replace spaces with underscores.
+//        System.out.println("Enter a string with spaces : ");
+//        String str = sc.nextLine();
+//        System.out.println(str.replace(" ","_"));
+
+//        Output : I am  manish
+//                 I_am__manish
+
+//        Q 2 : Write a Java program to fill in a letter template which looks like below:
+            // letter = “Dear <|name|>, Thanks a lot”
+            // Replace <|name|> with a string (some name)
+
+//        String letter = "Dear name, Thanks a lot";
+//        System.out.println("Enter your name: ");
+//        String name = sc.nextLine();
+//        System.out.println(letter.replace("name",name));
+//        !! We can replace with a string present in a variable !!
+
+
+//        Q 3 : Write a Java program to detect double and triple spaces in a string.
+//        I made it complicated but that's fine XD
+
+//        System.out.println("Enter a String with can contain double or triple space : ");
+//        String str = sc.nextLine();
+//        int space2 = str.indexOf("  ");
+//        int space3 = str.indexOf("   ");
+//
+//        if (space2 != -1 && space3 != -1)
+//        {
+//            System.out.println("There are double and triple spaces in the above string.\nDouble Space : "+space2+"\nTriple Space : "+space3);
+//        } else if(space2 == -1 && space3 != -1)
+//        {
+//            System.out.println("There are no double, But there are triple spaces in the above string.\nTriple Space : "+space3);
+//        }else if(space3 == -1 && space2 != -1)
+//        {
+//            System.out.println("There are no triple, But there are double spaces in the above string.\nDouble Space : "+space2);
+//        } else {
+//            System.out.println("There are no double or triple space.");
+//        }
+
+
+//         Q 4 : Write a program to format the following letter using escape sequence characters.
+//                Letter = “Dear Harry, This Java Course is nice. Thanks”
+//        String Letter = "Dear Harry, This Java Course is nice. Thanks";
+//        Letter = "Dear Harry,\nThis Java Course is nice.\nThanks";
+//        System.out.println(Letter);
+
+
+
+
+
+    }
+}

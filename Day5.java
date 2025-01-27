034ddf1e430069242a9cbae064432a0bccd57f3d
Manish Pawar <mspawar4620@gmail.com>
2025-01-27 21:09:29 +0530

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Day5 {
+    public static void main(String[] agrs){
+
+        Scanner sc = new Scanner(System.in);
+
+        /*Today I am doing some code form geeksforgeeks because
+        code with harry's 16,17 is on if- else statement and i know about that all*/
+        //So today I am doing some practice to solve some problem statements.
+
+        // Q 1 : Java Program to Add Two Binary Strings
+//        System.out.println("Enter two binary numbers : ");
+//        String n1 = sc.next();
+//        String n2 = sc.next();
+//        System.out.println("Adding them....");
+//        int a = Integer.parseInt(n1,2);
+//        int b = Integer.parseInt(n2,2);
+//        int sum = a+b;
+//        String res = Integer.toBinaryString(sum);
+//        System.out.println(res);
+//      Here, we use a method from a Wrapper Class. A Wrapper Class is
+//      a class that provides an object representation for a corresponding primitive data type.
+//      es, toBinaryString() comes under the Integer wrapper class in Java.
+//
+//      The Integer class is part of the wrapper classes in Java,
+//      and it provides various utility methods for working with integers, including toBinaryString(), parseInt(), valueOf(), and more.
+//        The Integer.toBinaryString() method in Java is a built-in function that converts
+//        a given integer (in decimal form) into its binary representation as a String.
+
+//        // Q 2 : Write a program to count the number of uppercase and lowercase letters in a given string.
+//        System.out.println("Enter a String : ");
+//        String str = sc.nextLine();
+//        int len = str.length();
+//        int countUP = 0;
+//        int countLP = 0;
+//        for(int i = 0; i < len; i++) {
+//            if (Character.isUpperCase(str.charAt(i))) {
+//                countUP++;
+//            } else if (Character.isLowerCase(str.charAt(i))) {
+//                countLP++;
+//            }
+//        }
+//        System.out.println("Number of upperCase : "+countUP+"\nNumber of lowerCase : "+countLP);
+
+
+    }
+}

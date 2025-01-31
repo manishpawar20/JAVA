3720defc8fb6ded3589f6d36409166722bf4ece9
Manish Pawar <mspawar4620@gmail.com>
2025-01-31 21:09:15 +0530

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+public class Day9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        Today I am learning array in java
+//        Concept is same as other languages
+//        Array is a collection of similar types of data
+
+//        There are three ways to create an array
+//        1 . Declaration and memory allocation
+//        int [] marks = new int [5];
+
+//        2 . Declaration and then memory allocation
+//        int [] marks;
+//        marks = new int[5];
+
+//        3 . Declaration,memory allocation and initialization together
+//        int [] marks = {1,2,3,4,5};
+
+//        we can create an array of any datatype float,String or int.
+//        String [] name = new String[5];
+//        float [] marks = new float[5];
+
+//        To accesses or to print or to insert we can use for loop
+//        System.out.println("Enter five names : ");
+//        for(int i = 0;i < name.length;i++)
+//        {
+//            name[i] = sc.next();
+//        }
+//
+//        System.out.println("Enter marks of five people: ");
+//        for(int i = 0;i < marks.length;i++)
+//        {
+//            marks[i] = sc.nextFloat();
+//        }
+//
+//        System.out.println("Details : ");
+//        for(int i = 0;i < name.length;i++)
+//        {
+//            System.out.println("Name : "+name[i]+"\nMarks : "+marks[i]);
+//        }
+
+
+//        We can accesses with for-each loop
+//        It prints all the elements
+//        int [] yo = {1,2,3,4,5};
+//        for (int element : yo)
+//        {
+//            System.out.println(element);
+//        }
+
+//        We can also create 2-D or 3-D array
+//        for 2-D array assume we have a building and we have save the each flat room number.
+//        example:
+//        int [][] flats = new int[2][3]; // we can create same for 3-D array (int [][][] flats = new int [1][1][1])
+//        flats [0][0] = 101;
+//        flats [0][1] = 102;
+//        flats [0][2] = 103;
+//        flats [1][0] = 201;
+//        flats [1][1] = 202;
+//        flats [1][2] = 203;
+//
+//        for (int i = 0; i<flats.length;i++)
+//        {
+//            for (int j = 0;j<flats[i].length;j++)
+//            {
+//                System.out.print(flats[i][j]+" ");
+//            }
+//            System.out.println("");
+//        }
+//        Output :
+//        101 102 103
+//        201 202 203
+
+    }
+}
